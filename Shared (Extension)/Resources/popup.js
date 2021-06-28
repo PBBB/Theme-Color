4061278c0ff814382278e58d3e6bb6883d032ce4
PBBB <pbbsignup@outlook.com>
2021-06-28 22:37:22 +0800

    Add Vue.js support

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,18 @@
 console.log("Hello World!", browser);
+const EventHandling = {
+        data() {
+            return {
+                message: 'Hello Vue.js!'
+            }
+        },
+        methods: {
+            reverseMessage() {
+                this.message = this.message
+                    .split('')
+                    .reverse()
+                    .join('')
+            }
+        }
+    }
+
+    Vue.createApp(EventHandling).mount('#event-handling')
