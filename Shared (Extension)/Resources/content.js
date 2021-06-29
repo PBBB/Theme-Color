9015a1dc8b3c3a7fc2460da19a83d9702972770e
PBBB <pbbsignup@outlook.com>
2021-06-30 00:01:14 +0800

    trying to get message going (and still not working)

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,11 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+//browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+//    console.log("Received response: ", response);
+//});
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+browser.runtime.onMessage.addListener((request) => {
     console.log("Received request: ", request);
 });
+
+//function changeColor(color) {
+//    document.querySelector("meta[name=theme-color]").setAttribute("content", color);
+//}
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,18 +1,24 @@
-console.log("Hello World!", browser);
-const EventHandling = {
-        data() {
-            return {
-                message: 'Hello Vue.js!'
-            }
-        },
-        methods: {
-            reverseMessage() {
-                this.message = this.message
-                    .split('')
-                    .reverse()
-                    .join('')
-            }
+const app = Vue.createApp({})
+app.component('color-option', {
+    props: {
+        isCurrent: Boolean,
+        color: String
+    },
+    methods: {
+        changeColor(color) {
+//            browser.tabs.getCurrent().then(tabInfo => {
+//                console.log("tanbinfo" + tabInfo);
+//            });
+            browser.runtime.sendMessage({type: "Word count request"}).then(value => {
+                console.log("success" + value); // Success!
+              }, reason => {
+                console.error("error" + reason); // Error!
+              });
+            console.log("send1")
         }
-    }
-
-    Vue.createApp(EventHandling).mount('#event-handling')
+    },
+    template: `
+        <div :style="{background: color}" @click.native="changeColor(color)"></div>
+        `
+    })
+app.mount('#color-demo')
